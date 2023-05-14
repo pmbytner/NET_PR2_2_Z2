fd21d59ee1da70c094f0dd3d33c81c730abbe40f
PMBytner <pawel.m.bytner@gmail.com>
2023-05-14 19:36:48 +0200

    import eksport

--- NET_PR2_2_Z2/OknoListyOsób.xaml.cs
+++ NET_PR2_2_Z2/OknoListyOsób.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml.Serialization;
 
 namespace NET_PR2_2_Z2;
 /// <summary>
@@ -19,11 +21,13 @@ namespace NET_PR2_2_Z2;
 /// </summary>
 public partial class OknoListyOsób : Window
 {
-	private ObservableCollection<Osoba> KolekcjaOsób = new();
+	private const string ŚcieżkaIO = "listaOsób.xml";
+	private readonly ObservableCollection<Osoba> KolekcjaOsób = new();
 	//ListBox ListaOsób;
 	public OknoListyOsób()
 	{
 		DataContext = KolekcjaOsób;
+		Import(this, new RoutedEventArgs());
 		InitializeComponent();
 	}
 
@@ -48,5 +52,28 @@ public partial class OknoListyOsób : Window
 		KolekcjaOsób.Remove(
 			(Osoba)ListaOsób.SelectedItem
 			);
+	}
+
+	private void Eksport(object sender, RoutedEventArgs e)
+	{
+		XmlSerializer serializator = new XmlSerializer(typeof(ObservableCollection<Osoba>));
+		TextWriter strumieńZapisu = new StreamWriter(ŚcieżkaIO);
+		serializator.Serialize(
+			strumieńZapisu,
+			KolekcjaOsób
+			);
+		strumieńZapisu.Close();
+	}
+
+	private void Import(object sender, RoutedEventArgs e)
+	{
+		XmlSerializer serializator = new XmlSerializer(typeof(ObservableCollection<Osoba>));
+		FileStream plik = new FileStream(ŚcieżkaIO, FileMode.Open);
+		ObservableCollection<Osoba> wczytane
+			= (ObservableCollection<Osoba>) serializator.Deserialize(plik);
+		KolekcjaOsób.Clear();
+		foreach(Osoba osoba in wczytane)
+			KolekcjaOsób.Add(osoba);
+		plik.Close();
 	}
 }
